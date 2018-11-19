95afdd596af0a40f7629d41ff09ee29fcf6b11bc
Angela Lima <angelawasadiver@gmail.com>
2018-11-19 11:54:57 -0300

    Tela de destino

---
+++ Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/TelaPrincipalMapa/DestinoViagem.java
@@ -0,0 +1,13 @@
+package projetosendwithmemotorista.sendwithmemotorista.Activity.TelaPrincipalMapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DestinoViagem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destino_viagem);
+    }
+}
