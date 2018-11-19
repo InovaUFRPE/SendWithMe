e571b177dc8176cb1c8df04ef9e84ffc3b08b118
Angela Lima <angelawasadiver@gmail.com>
2018-11-19 12:23:20 -0300

    Telas início, perfil e lista de viagens
    activity_home.xml, activity_editar_perfil.xml e activity_lista_viagens.xml prontos

---
+++ Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/TelaPrincipalMapa/ListaViagens.java
@@ -0,0 +1,13 @@
+package projetosendwithmemotorista.sendwithmemotorista.Activity.TelaPrincipalMapa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaViagens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_viagens);
+    }
+}
---
+++ usuario/app/src/main/java/com/example/emano/sendwithme/ViagemPackage/ListaViagens.java
@@ -0,0 +1,13 @@
+package com.example.emano.sendwithme.ViagemPackage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaViagens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_viagens);
+    }
+}
