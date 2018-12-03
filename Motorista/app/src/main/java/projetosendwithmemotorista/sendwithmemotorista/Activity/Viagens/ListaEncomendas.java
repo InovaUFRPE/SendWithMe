1091bc4e030b3c3e1b0d0a4f00e4781147c561bc
Acadi <ac.adi@hotmail.com>
2018-12-02 21:53:31 -0300

    ajustes incompleto tela listar encomendas

--- Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/Viagens/ListaEncomendas.java
+++ Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/Viagens/ListaEncomendas.java
@@ -1,15 +1,27 @@
 package projetosendwithmemotorista.sendwithmemotorista.Activity.Viagens;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import projetosendwithmemotorista.sendwithmemotorista.Helper.PreferenciasAndroid;
 import projetosendwithmemotorista.sendwithmemotorista.R;
 
 public class ListaEncomendas extends AppCompatActivity {
@@ -30,6 +42,9 @@ public class ListaEncomendas extends AppCompatActivity {
     private EditText edtassdisp;
 
     private CheckBox encomendabox;
+    private ListView listaencomendas;
+    private ArrayList<String> lista;
+    private ArrayAdapter<String> adapter;
 
 
 
@@ -38,6 +53,8 @@ public class ListaEncomendas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_encomendas);
         btnavancar = findViewById(R.id.btnavancar);
+        listaencomendas = findViewById(R.id.listaviagens);
+        setardadoslistaencomendas();
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -59,6 +76,35 @@ public class ListaEncomendas extends AppCompatActivity {
 
         });
 
+    }
+
+    private void setardadoslistaencomendas(){
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Encomendas");
+        lista = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this,R.layout.infoviagens,R.id.textView8, lista);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    Encomendas encomendas = ds.getValue(Encomendas.class);
+                   // String userId = String.valueOf(encomendas.getUsuarioid());
+                   // final PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(ListaEncomendas.this);
+                   // if (userId.equals(preferenciasAndroid.getIdentificador()))
+                    lista.add(" Cidade Origem: " + encomendas.getCidadeOrigem() +"\n" + " Cidade Destino: "+ encomendas.getCidadeDestino());
+                    // lista.add(ds.getValue().toString());
+
+                }
+
+                listaencomendas.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void funcacanvar() {
