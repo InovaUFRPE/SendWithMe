ef05ecb3bea55b3296b23d1a41f7f13b5ca7c966
Emanoel Gomes <emanoel20092009@gmail.com>
2018-12-05 23:50:04 -0200

    Primeira parte da listagem de encomendas

---
+++ usuario/app/src/main/java/com/example/emano/sendwithme/EncomendaPackage/ListarEncomendas.java
@@ -0,0 +1,73 @@
+package com.example.emano.sendwithme.EncomendaPackage;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.emano.sendwithme.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ListarEncomendas extends AppCompatActivity {
+
+    private ArrayList<Encomenda> encomendas = new ArrayList<>();
+    private ArrayAdapter adapter;
+    private ListView lista;
+    private DatabaseReference databaseReference;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_encomendas);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Encomendas").child(user.getUid());
+
+        lista = (ListView) findViewById(R.id.encomenda_view);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                encomendas.clear();
+
+                for(DataSnapshot dados: dataSnapshot.getChildren()){
+
+                    Encomenda encomenda = dados.getValue(Encomenda.class);
+                    addOnEncomendaOnLista(encomenda);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        setAdapter();
+
+    }
+
+    private void addOnEncomendaOnLista(Encomenda encomenda){
+        this.encomendas.add(encomenda);
+    }
+
+    private void setAdapter(){
+        adapter = new ListarEncomendasAdapter(getApplicationContext(), this.encomendas);
+        adapter.notifyDataSetChanged();
+        lista.setAdapter(adapter);
+    }
+
+
+}
---
+++ usuario/app/src/main/java/com/example/emano/sendwithme/EncomendaPackage/ListarEncomendasAdapter.java
@@ -0,0 +1,45 @@
+package com.example.emano.sendwithme.EncomendaPackage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.emano.sendwithme.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class ListarEncomendasAdapter extends ArrayAdapter<Encomenda> {
+
+    private final ArrayList<Encomenda> encomendas;
+    private final Context context;
+
+    public ListarEncomendasAdapter(Context context, ArrayList<Encomenda> encomendas){
+
+        super(context, R.layout.linha_encomenda, encomendas);
+        this.context = context;
+        this.encomendas = encomendas;
+
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.linha_encomenda, parent, false);
+        TextView nome = (TextView) rowView.findViewById(R.id.txtNomeEncomendaLista);
+        TextView origem = (TextView) rowView.findViewById(R.id.txtOrigemEncomendaLista);
+        TextView destino = (TextView) rowView.findViewById(R.id.txtDestinoEncomendaLista);
+
+        nome.setText(encomendas.get(position).getNomeObjeto());
+        origem.setText(encomendas.get(position).getEnderecoOrigem());
+        destino.setText(encomendas.get(position).getEndrerecoDestino());
+
+        return rowView;
+
+    }
+
+}
