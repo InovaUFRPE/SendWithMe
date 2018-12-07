51c67ea8edb6811e61ccea1ff504521288fa1e99
Acadi <ac.adi@hotmail.com>
2018-12-07 17:16:14 -0300

    ajustes na lista de encomenda

--- Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/Viagens/HistoricoEncomendas.java
+++ Motorista/app/src/main/java/projetosendwithmemotorista/sendwithmemotorista/Activity/Viagens/HistoricoEncomendas.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,6 +18,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import projetosendwithmemotorista.sendwithmemotorista.Activity.CadastroMotorista.CadastroActivity;
+import projetosendwithmemotorista.sendwithmemotorista.Helper.PreferenciasAndroid;
 import projetosendwithmemotorista.sendwithmemotorista.R;
 
 public class HistoricoEncomendas extends AppCompatActivity {
@@ -30,8 +33,8 @@ public class HistoricoEncomendas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico_encomendas);
-        listaencomendas = findViewById(R.id.listaHistoricoEncomendas);
         setardadoslistaencomendas();
+        listaencomendas = findViewById(R.id.listaHistoricoEncomendas);
 
         btnvoltar = findViewById(R.id.Btnavanc);
         btnvoltar.setOnClickListener(new View.OnClickListener() {
@@ -50,18 +53,28 @@ public class HistoricoEncomendas extends AppCompatActivity {
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("EncomendasSelecionadas");
         lista = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this,R.layout.infoviagens,R.id.textView8, lista);
+        adapter = new ArrayAdapter<String>(this,R.layout.infoencomendas,R.id.textViewHistEncomendas, lista);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot ds: dataSnapshot.getChildren()) {
-                    // Encomendas encomendas = ds.getValue(Encomendas.class);
-                    lista.add(ds.getKey());
-                    //setardadoslistaencomendas2();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    EncomendasSelecionadas encomendasSelecionadas = ds.getValue(EncomendasSelecionadas.class);
+                    String userId = String.valueOf(encomendasSelecionadas.getIdMotorista());
+                    final PreferenciasAndroid preferenciasAndroid = new PreferenciasAndroid(HistoricoEncomendas.this);
+                    String idMotoristaLogado = preferenciasAndroid.getIdentificador();
+                    if (userId.equals(idMotoristaLogado)) {
+                        lista.add(" Cidade de Origem da viagem :" + encomendasSelecionadas.getCidadeOrigemViagem());
+                        //setardadoslistaencomendas2();
+                     //   Toast.makeText(HistoricoEncomendas.this, "TO NO IF" + userId + "logado"+ idMotoristaLogado, Toast.LENGTH_LONG).show();
+                        listaencomendas.setAdapter(adapter);
+                    } else {
+
+                      //  Toast.makeText(HistoricoEncomendas.this, "NAOOOOO", Toast.LENGTH_LONG).show();
+
+                    }
+
 
                 }
-
-
             }
 
             @Override
